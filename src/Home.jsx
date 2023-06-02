369d326ed9bfe88e153537d280ecf217daaa7c46
victorfernandesraton <vfbraton@gmail.com>
2023-06-02 00:19:48 -0300

    create home

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,21 @@ import Nullstack from 'nullstack';
 import PostList from './PostList';
 
 class Home extends Nullstack {
-  
+
   render() {
     return (
       <main class='max-w-[900px] mx-auto'>
-        <h1 class='text-5xl font-bold mt-8 mb-16'>
-          Home
-        </h1>
+
+        <div class='flex flex-col-reverse md:flex-row gap-x-16'>
+          <div class="flex flex-col">
+            <h1 class='text-5xl font-bold mt-8 mb-8 sm:mb-16'>
+              Bem vindo ao meu caos
+            </h1>
+            <p class='text-xl my-4'>Fala guys, sou Victor Raton, desenvolvedor fullstack dedsde 2019, defensor ferrenho de open-source e criador exímio de bugs em javascript</p>
+          </div>
+          <img src='assets/img/profile.webp' class="w-44 py-4 rounded-full self-center" />
+        </div>
+        <h2 class='text-4xl font-bold text-rosePine-rose my-8'>O que andei aprontando </h2>
         <PostList persistent />
       </main>
     )
