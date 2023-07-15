ad41c471bda14e40e5668a1a5aa65fc45a04636a
victorfernandesraton <vfbraton@gmail.com>
2023-07-15 07:34:35 -0300

    improve some custon changes

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
+import Nullstack from 'nullstack'
+
 import { readdirSync } from 'node:fs'
 import path from 'node:path'
 
-import Nullstack from 'nullstack'
 import { MarkedAdapter } from './lib/marked/MarkedAdapter'
-
 import Application from './src/Application'
 
 const context = Nullstack.start(Application)
 
-const { worker } = context;
+const { worker } = context
 
-const articles = readdirSync(path.join(__dirname, '../posts',));
+const articles = readdirSync(path.join(__dirname, '../posts'))
 
-
-worker.preload = [
-  "/",
-  "/me",
-  "/blog",
-  ...articles.map((article) => '/blog/' + article.replace('.md', '')),
-]
+worker.preload = ['/', '/me', '/blog', ...articles.map((article) => `/blog/${article.replace('.md', '')}`)]
 context.start = async function start() {
   context.marked = MarkedAdapter._start()
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,28 +1,32 @@
-import Nullstack from 'nullstack';
-import './Post.scss';
-import { DateTimeNormalizer } from '../lib/normalizer/DateTimeNormalizer';
-import { existsSync, readFileSync } from 'node:fs';
+import Nullstack from 'nullstack'
 import fm from 'front-matter'
+import { existsSync, readFileSync } from 'node:fs'
 import fs from 'node:fs/promises'
 import path from 'node:path'
-import { MarkedAdapter } from '../lib/marked/MarkedAdapter';
+
+import "./Post.scss"
+import 'highlight.js/styles/tokyo-night-dark.css'
+
+import { MarkedAdapter } from '../lib/marked/MarkedAdapter'
+import { DateTimeNormalizer } from '../lib/normalizer/DateTimeNormalizer'
 
 class Post extends Nullstack {
+
   static async getPost({ key, marked }) {
-    const path = `posts/${key}.md`;
+    const path = `posts/${key}.md`
     if (!existsSync(path)) {
-      return null;
+      return null
     }
-    let data = readFileSync(path, 'utf-8');
-    data = MarkedAdapter.replaceImageUrl({ md: data });
+    let data = readFileSync(path, 'utf-8')
+    data = MarkedAdapter.replaceImageUrl({ md: data })
 
     const { attributes, body } = fm(data)
     const html = marked.parse(body)
     return {
       html,
       name: key,
-      ...attributes
-    };
+      ...attributes,
+    }
   }
 
   static async getAllPost(context) {
@@ -34,26 +38,26 @@ class Post extends Nullstack {
       const fileStats = await fs.stat(filePath)
 
       if (fileStats.isFile() && path.extname(file) === '.md') {
-        const data = await Post.getPost({ ...context, key: file.replace('.md', ''), })
+        const data = await Post.getPost({ ...context, key: file.replace('.md', '') })
         filteredFiles.push(data)
       }
     }
     return filteredFiles
   }
+
   async initiate({ page, params, router }) {
     const article = await Post.getPost({
       key: params.slug !== '' ? params.slug : router.path.slice(1),
-    });
+    })
 
-
-    page.title = article.title;
+    page.title = article.title
     if (article?.description) {
-      page.description = article.description;
+      page.description = article.description
     }
     if (article?.cover) {
-      page.image = article.cover.replace('/public', '');
+      page.image = article.cover.replace('/public', '')
     }
-    Object.assign(this, article);
+    Object.assign(this, article)
   }
 
   render({ router }) {
@@ -65,12 +69,13 @@ class Post extends Nullstack {
       <>
         <header class="mx-auto mb-16 mt-8 max-w-[900px] flex flex-col gap-y-4 content-between break-words">
           <h1 class="text-4xl font-bold text-rosePine-love">{this?.title}</h1>
-          {this.description && (
-            <h2 class="text-2xl font-bold text-rosePine-gold mb-4">{this?.description}</h2>
-          )}
-          <p class="text-sm font-semibold text-rosePine-foam">Published at {DateTimeNormalizer.dateToTimeAgo(this.published_at)}</p>
+          {this.description && <h2 class="text-2xl font-bold text-rosePine-gold mb-4">{this?.description}</h2>}
+          <p class="text-sm font-semibold text-rosePine-foam">
+            Published at {DateTimeNormalizer.dateToTimeAgo(this.published_at)}
+          </p>
         </header>
         <article html={this.html} class="mx-auto max-w-[900px] article-custon-style" />
+        <div class="text-rosePine-iris list-disc pl-4 marker:text-rosePine-iris underline" />
       </>
     )
   }
