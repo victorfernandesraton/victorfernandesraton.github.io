4ce97472f81633a0465dd79421885a67ca3a3839
victorfernandesraton <vfbraton@gmail.com>
2023-08-17 19:06:42 -0300

    automate: lint

--- client.js
+++ client.js
@@ -3,6 +3,4 @@ import Nullstack from 'nullstack'
 import Application from './src/Application'
 const context = Nullstack.start(Application)
 
-context.start = async function start(context) {}
-
 export default context
