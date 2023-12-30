18ff2aa8a433d0064efd0f06a3b4e0fca2115a8f
Victor Raton <vfbraton@gmail.com>
2023-12-29 22:01:19 -0300

    feat: improve some details to render all app

--- src/Application.jsx
+++ src/Application.jsx
@@ -12,73 +12,73 @@ import NotFound from './NotFound.jsx'
 import Post from './Post.jsx'
 class Application extends Nullstack {
 
-  prepare(context) {
-    context.page.title = `${context.project.shortName}`
-    context.page.description = this.description
-    context.page.image = `/assets/img/profile.webp`
-  }
-
-  renderHead() {
-    return (
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          as="style"
-        />
-      </head>
-    )
-  }
-
-  renderFooter() {
-    return (
-      <footer class="pt-6 flex flex-col max-w-[900px] mx-auto my-8 inset-x-0 bottom-0 lg:items-start items-center gap-4 text-center lg:text-start border-t-rosePine-surface border-t-[1px]">
-        <p>Developed with &#128156; by victorfernandesraton</p>
-        <a href="https://nullstack.app/">
-          <p>Powered by</p>
-          <Logo height={20} light />
-        </a>
-      </footer>
-    )
-  }
-
-  renderBody({ children }) {
-    return (
-      <>
-        <Head />
-        <Navbar persist />
-        <body class="bg-rosePine-base text-rosePine-text lg:px-0 px-4 h-fulli">{children}</body>
-        <Footer />
-      </>
-    )
-  }
-
-  render({ router, page }) {
-    if (page.status === 404) {
-      router.path = '/404'
+    prepare(context) {
+        context.page.title = `${context.project.shortName}`
+        context.page.description = this.description
+        context.page.image = `/assets/img/profile.webp`
     }
 
-    if (!this.initiated) {
-      return (
-        <Body>
-          <main class="flex items-center justify-center align-middle max-w-[900px] mx-auto py-32">
-            <Loading />
-          </main>
-        </Body>
-      )
+    renderHead() {
+        return (
+            <head>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+                    as="style"
+                />
+            </head>
+        )
     }
 
-    return (
-      <Body>
-        <Home route="/" />
-        <Blog route="/blog" />
-        <Post route="/blog/:slug" key={router.path} />
-        <Blog route="/tag/:tag" key={router?.params?.tag} />
-        <About route="/me" />
-        <NotFound route="/404" />
-      </Body>
-    )
-  }
+    renderFooter() {
+        return (
+            <footer class="pt-6 flex flex-col max-w-[900px] mx-auto my-8 inset-x-0 bottom-0 lg:items-start items-center gap-4 text-center lg:text-start border-t-rosePine-surface border-t-[1px]">
+                <p>Developed with &#128156; by victorfernandesraton</p>
+                <a href="https://nullstack.app/">
+                    <p>Powered by</p>
+                    <Logo height={20} light />
+                </a>
+            </footer>
+        )
+    }
+
+    renderBody({ children }) {
+        return (
+            <>
+                <Head />
+                <Navbar persist />
+                <body class="bg-rosePine-base text-rosePine-text lg:px-0 px-4 h-fulli">{children}</body>
+                <Footer />
+            </>
+        )
+    }
+
+    render({ router, page }) {
+        if (page.status === 404) {
+            router.path = '/404'
+        }
+
+        if (!this.initiated) {
+            return (
+                <Body>
+                    <main class="flex items-center justify-center align-middle max-w-[900px] mx-auto py-32">
+                        <Loading />
+                    </main>
+                </Body>
+            )
+        }
+
+        return (
+            <Body>
+                <Home route="/" />
+                <Blog route="/blog" />
+                <Post route="/blog/:slug" key={router.path} />
+                <Blog route="/tag/:tag" key={router?.params?.tag} />
+                <About route="/me" />
+                <NotFound route="/404" />
+            </Body>
+        )
+    }
 
 }
 
