71c360d92da62903e777354a23cfda47852ca0a5
Victor Raton <overlord.palace235@aleeas.com>
2025-10-31 01:49:58 -0300

    fix: tags de experiencia

---
+++ _includes/xp.tsx
@@ -0,0 +1,46 @@
+
+export default function (
+  data: Lume.Data,
+) {
+  const {
+    title,
+    children,
+    tags,
+    description,
+    sitename,
+    comp,
+    alternates,
+    lang,
+    nav,
+  } = data;
+
+
+  return (
+    <>
+      {{ __html: "<!DOCTYPE html>" }}
+      <header>
+        <title>
+          {`${sitename} - ${title}`}
+        </title>
+      </header>
+      <html>
+        <comp.Navbar nav={nav} alternates={alternates} lang={lang} />
+        <body>
+         
+          <h1>{title}</h1>
+          <h2>{description}</h2>
+          <ul>
+            {tags.map((item, index) => (
+              <li key={index}>
+                <a href={`/tag/${item}`}>{item}</a>
+              </li>
+            ))}
+          </ul>
+          {children}
+
+          <comp.Footer />
+        </body>
+      </html>
+    </>
+  );
+}
