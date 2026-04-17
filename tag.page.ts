b0b4a4dd3c02ec0c799fd6a9958421d11f63297a
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2026-04-17 17:01:28 -0300

    fix: correção de página de tags

--- tag.page.ts
+++ tag.page.ts
@@ -2,10 +2,11 @@ const capitalized = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 export default function* ({ search, lang }: Lume.Data) {
   const tags = search.values<string>(`tags`, `lang=${lang}`);
   for (const tag of tags) {
+    const tagValue = tag.toLowerCase()
     const data = {
-      url: `/tag/${tag}/`,
+      url: `/tag/${tagValue}/`,
       layout: "tag.tsx",
-      title: capitalized(tag),
+      title: capitalized(tagValue),
       result: tag,
       lang,
     };
